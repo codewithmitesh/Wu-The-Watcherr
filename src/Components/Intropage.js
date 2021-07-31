7dd6ce9b412fefe587693a02923dfbbbae829bb5
Mitesh Tank <codewithmitesh@gmail.com>
2021-08-01 00:33:01 +0530

    adding slightly parallex

--- src/Components/Intropage.js
+++ src/Components/Intropage.js
@@ -1,12 +1,33 @@
 import React from "react";
 import "../Components/intropage.css";
-export default function Intropage (){
+import bg from "../Components/Background.png";
+import ps from "../Components/planet-small.png";
+import pb from "../Components/planet-big.png";
+export default function Intropage() {
+  let pls = document.getElementById("Planet-small");
+  let stars = document.getElementById("Bg-Image");
+  let plb = document.getElementById("Planet-big");
+  let text = document.getElementById("text");
+  let btn = document.getElementById("btn");
+
+  window.addEventListener("scroll", function () {
+    let value = this.window.scrollY;
+    pls.style.top = value * 0.65 + "px";
+    plb.style.left = value * -0.5 + "px";
+  });
+
   return (
     <div>
       <div className="Intropage">
-        <h3>Hello Wu!</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur maiores modi, consequatur quis odit rem iste ipsa incidunt quidem dignissimos debitis quaerat deserunt facilis eius at ea nobis consequuntur illum.</p>
+        <h2 id="text">Wu - Watcher </h2>
+        <a href="#content" id="btn">
+          {" "}
+          Explore{" "}
+        </a>
+        <img src={bg} id="Bg-Image" alt="background" />
+        <img src={ps} id="Planet-small" alt="background" />
+        <img src={pb} id="Plante-big" alt="background" />
       </div>
     </div>
   );
-};
+}
--- src/Components/Watcher.js
+++ src/Components/Watcher.js
@@ -19,7 +19,12 @@ export default function Watcher() {
 
   return (
     <>
-      <div className="content">
+      <div className="content" id = "content">
+      <div className="wu-text">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat velit sunt quae. Labore voluptas mollitia autem nobis dolore doloremque. Minima odit cupiditate atque, nostrum dignissimos beatae assumenda corporis optio quo?
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat perferendis, alias cumque dolor velit repellendus quo sequi dignissimos, distinctio debitis recusandae minima est dolore id at non et doloremque. Maxime architecto reiciendis ullam exercitationem tempore, odio id cumque fuga quae impedit dolorem vitae, aliquam necessitatibus sunt adipisci magni porro. Quisquam vitae sunt modi quis nobis? Corrupti quia esse accusamus minus asperiores quis sunt minima dolore commodi repudiandae quibusdam error ipsa, officiis, ad dolores illo reiciendis enim, nam temporibus! Quos laudantium dicta placeat nemo odit doloremque molestiae tempora totam ullam, explicabo ea quaerat aliquam numquam accusamus perferendis reprehenderit aspernatur adipisci odio deserunt! Voluptates officiis amet obcaecati ipsam porro rem animi atque, quia maiores cumque fugiat expedita eaque enim dolore id, nemo itaque vitae quibusdam! Deserunt voluptate, nesciunt eveniet maiores necessitatibus esse et repudiandae tempora voluptatum quidem eius quaerat. Perspiciatis enim harum cumque quibusdam eos accusantium quasi corporis animi dolorem at cum, temporibus totam, voluptatem fuga atque esse ipsa expedita, maxime incidunt? Aut dolore culpa harum aliquid necessitatibus amet, voluptatum eius quidem saepe ipsa ratione laborum a debitis atque, itaque ipsam. Molestiae nam, illo saepe hic architecto sunt fugiat similique tempora sed deleniti accusamus adipisci nisi eveniet omnis? Suscipit magni odio aspernatur.
+
+      </div>  
         <div className="box">
           <div className="inputData">
             <input
